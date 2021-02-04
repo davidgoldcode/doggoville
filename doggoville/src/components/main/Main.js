0d8c02c47819ab95de21af6a049de586eca7792e
David Gold <davidgold@hey.com>
2021-02-04 16:42:09 -0500

    html & css for modal in place. Need to trim

--- doggoville/src/components/main/Main.js
+++ doggoville/src/components/main/Main.js
@@ -1,10 +1,12 @@
 import { Gallery } from "../gallery";
+import { Search } from "../search";
+import { Modal } from "../modal";
 
 const Main = () => {
   return (
     <div className="md:col-start-3	md:col-span-3">
-      {/* search component */}
-      {/* modal component */}
+      <Search />
+      <Modal />
       <Gallery />
     </div>
   );
