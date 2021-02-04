9bc10b04fcb21742a0d8c5b1d3157c4c9ad34e7e
David Gold <davidgold@hey.com>
2021-02-04 16:27:46 -0500

    Gallery component basics in place. Working with scroll

---
+++ doggoville/src/components/main/Main.js
@@ -0,0 +1,13 @@
+import { Gallery } from "../gallery";
+
+const Main = () => {
+  return (
+    <div className="md:col-start-3	md:col-span-3">
+      {/* search component */}
+      {/* modal component */}
+      <Gallery />
+    </div>
+  );
+};
+
+export default Main;
